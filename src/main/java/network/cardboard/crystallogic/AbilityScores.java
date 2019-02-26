0a6ed7353b79a8d710d23341ee2c48236e64741f
Dave Chesser <dchesser@ggc.edu>
2019-02-25 22:07:52 -0500

    new: building ability scores with die rolls
    The Pathfinder game specifies a few ways to randomly generate a set of
    AbilityScores: the classical way, the modern way, and the heroic way.
    
    The classical way, dating back to the earlier AD&D days, involve
    rolling three six-sided dice (3d6) and summing the results.
    The modern way, the way modern D&D does it as of the fifth edition and
    Pathfinder recommends, is to roll 4d6 and not include the smallest roll.
    This often gets you better scores than the classical way.
    The heroic way is to simply roll 2d6 and add 6 to the result.
    This often gets you better scores than the modern way.
    
    To call, import the BuildMethod and use one to roll ability scores.
    
    import network.cardboard.crystallogic.AbilityScores.BuildMethod;
    // ...
    AbilityScores stats = new AbilityScores(BuildMethod.MODERN);

--- src/main/java/network/cardboard/crystallogic/AbilityScores.java
+++ src/main/java/network/cardboard/crystallogic/AbilityScores.java
@@ -1,5 +1,17 @@
 package network.cardboard.crystallogic;
 
+import java.util.IntSummaryStatistics;
+import java.util.stream.IntStream;
+
+/**
+ * Ability Scores represent a Player Character's ability to interact
+ * with the world around them. These ability scores will affect feats
+ * and skills for the player to accomplish. The six ability scores are
+ * strength (lifting and swinging swords), dexterity (armo rand
+ * shooting bows),constitution (general health and hit points),
+ * intelligence (knowledge and magic), wisdom (willpower and
+ * awareness), and charisma (speechcraft animals).
+ */
 public class AbilityScores
 {
     public AbilityScore strength;
@@ -9,80 +21,152 @@ public class AbilityScores
     public AbilityScore wisdom;
     public AbilityScore charisma;
 
+    public static enum BuildMethod {
+        CLASSICAL,              // 3d6
+        MODERN,                 // 4d6 - minimum value
+        HEROIC,                 // 2d6 + 6
+    }
+
     public AbilityScores()
     {
-	this.strength = new AbilityScore(0);
-	this.dexterity = new AbilityScore(0);
-	this.constitution = new AbilityScore(0);
-	this.intelligence = new AbilityScore(0);
-	this.wisdom = new AbilityScore(0);
-	this.charisma = new AbilityScore(0);
+        this.strength = new AbilityScore(0);
+        this.dexterity = new AbilityScore(0);
+        this.constitution = new AbilityScore(0);
+        this.intelligence = new AbilityScore(0);
+        this.wisdom = new AbilityScore(0);
+        this.charisma = new AbilityScore(0);
+    }
+
+    public AbilityScores(BuildMethod method)
+    {
+        int[] scores = new int[6];
+
+	// Makes me wish there was Haskell's pattern matching here.
+        switch (method) {
+        case CLASSICAL:
+            scores = IntStream
+                .generate(AbilityScores::rollClassicalMethod)
+                .limit(6)
+                .toArray();
+            break;
+        case MODERN:
+            scores = IntStream
+                .generate(AbilityScores::rollModernMethod)
+                .limit(6)
+                .toArray();
+            break;
+        case HEROIC:
+            scores = IntStream
+                .generate(AbilityScores::rollHeroicMethod)
+                .limit(6)
+                .toArray();
+            break;
+        }
+
+        this.strength = new AbilityScore(scores[0]);
+        this.dexterity = new AbilityScore(scores[1]);
+        this.constitution = new AbilityScore(scores[2]);
+        this.intelligence = new AbilityScore(scores[3]);
+        this.wisdom = new AbilityScore(scores[4]);
+        this.charisma = new AbilityScore(scores[5]);
     }
 
     public AbilityScores(int defaultValue)
     {
-	this.strength = new AbilityScore(defaultValue);
-	this.dexterity = new AbilityScore(defaultValue);
-	this.constitution = new AbilityScore(defaultValue);
-	this.intelligence = new AbilityScore(defaultValue);
-	this.wisdom = new AbilityScore(defaultValue);
-	this.charisma = new AbilityScore(defaultValue);
+        this.strength = new AbilityScore(defaultValue);
+        this.dexterity = new AbilityScore(defaultValue);
+        this.constitution = new AbilityScore(defaultValue);
+        this.intelligence = new AbilityScore(defaultValue);
+        this.wisdom = new AbilityScore(defaultValue);
+        this.charisma = new AbilityScore(defaultValue);
     }
 
     public AbilityScores(int st, int dx, int cn,
-			 int in, int ws, int ch)
+                         int in, int ws, int ch)
     {
-	this.strength = new AbilityScore(st);
-	this.dexterity = new AbilityScore(dx);
-	this.constitution = new AbilityScore(cn);
-	this.intelligence = new AbilityScore(in);
-	this.wisdom = new AbilityScore(ws);
-	this.charisma = new AbilityScore(ch);
+        this.strength = new AbilityScore(st);
+        this.dexterity = new AbilityScore(dx);
+        this.constitution = new AbilityScore(cn);
+        this.intelligence = new AbilityScore(in);
+        this.wisdom = new AbilityScore(ws);
+        this.charisma = new AbilityScore(ch);
     }
 
     @Override
     public String toString()
     {
-	return String.format("STR %s\tDEX %s\tCON %s\t" +
-			     "INT %s\tWIS %s\tCHA %s",
-			     this.strength.toString(),
-			     this.dexterity.toString(),
-			     this.constitution.toString(),
-			     this.intelligence.toString(),
-			     this.wisdom.toString(),
-			     this.charisma.toString());
+        return String.format("STR %s\tDEX %s\tCON %s\t" +
+                             "INT %s\tWIS %s\tCHA %s",
+                             this.strength.toString(),
+                             this.dexterity.toString(),
+                             this.constitution.toString(),
+                             this.intelligence.toString(),
+                             this.wisdom.toString(),
+                             this.charisma.toString());
+    }
+
+    private static int rollClassicalMethod()
+    {
+        int sum = IntStream
+            .generate(Die.d6::roll)
+            .limit(3)
+            .sum();
+
+        return sum;
+    }
+
+    private static int rollModernMethod()
+    {
+        IntSummaryStatistics rolls = IntStream
+            .generate(Die.d6::roll)
+            .limit(4)
+            .summaryStatistics();
+        int sum = Math.toIntExact(rolls.getSum());
+        int min = rolls.getMin();
+
+        return (sum - min);
+    }
+
+    private static int rollHeroicMethod()
+    {
+        int sum = IntStream
+            .generate(Die.d6::roll)
+            .limit(2)
+            .sum();
+
+        return (sum + 6);
     }
 
     public class AbilityScore
     {
-	int score;
+        int score;
 
-	private AbilityScore(int value)
-	{
-	    this.score = value;
-	}
+        private AbilityScore(int value)
+        {
+            this.score = value;
+        }
 
-	public int getValue()
-	{
-	    return this.score;
-	}
+        public int getValue()
+        {
+            return this.score;
+        }
 
-	public int setValue(int value)
-	{
-	    return this.score = value;
-	}
+        public int setValue(int value)
+        {
+            return this.score = value;
+        }
 
-	public int getModifier()
-	{
-	    // Thanks to William for correcting the formula.
-	    return (this.score / 2) - 5;
-	}
+        public int getModifier()
+        {
+            // Thanks to William for correcting the formula.
+            return (this.score / 2) - 5;
+        }
 
-	@Override
-	public String toString()
-	{
-	    return String.format("%d (%+d)",
-				 this.getValue(), this.getModifier());
-	}
+        @Override
+        public String toString()
+        {
+            return String.format("%d (%+d)",
+                                 this.getValue(), this.getModifier());
+        }
     }
 }
